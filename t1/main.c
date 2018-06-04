48ebcd8822ab4f9b228269197464cd5a8326ad49
philiptung <philip.dongfei@gmail.com>
2018-06-04 16:25:17 +0800

    configure demo

--- t1/main.c
+++ t1/main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
+#include "projectConfigure.h"
 
 int main(void)
 {
     printf("Hello World from t1 Main!\n");
+    printf("%s\n",PROJECT_BINARY_DIR);
 #ifdef DEBUG_BUILD
     int v = 10;
     printf("the value of v is %d\n", v);
