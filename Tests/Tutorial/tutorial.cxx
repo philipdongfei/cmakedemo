17928ed1a85b2276f12609f62e3cd87c66193225
philiptung <philip.dongfei@gmail.com>
2018-06-11 13:31:08 +0800

    Porting CMake to new Platforms and languages

--- Tests/Tutorial/tutorial.cxx
+++ Tests/Tutorial/tutorial.cxx
@@ -4,6 +4,10 @@
 #include <math.h>
 #include "TutorialConfig.h"
 
+#ifdef USE_MYMATH
+#include "MathFunctions.h"
+#endif
+
 int main (int argc, char *argv[])
 {
     if (argc < 2)
@@ -17,7 +21,12 @@ int main (int argc, char *argv[])
     }
 
     double inputValue = atof(argv[1]);
+#ifdef USE_MYMATH
+    
+    double outputValue = mysqrt(inputValue);
+#else
     double outputValue = sqrt(inputValue);
+#endif
     fprintf(stdout, "The square root of %g is %g\n",
             inputValue, outputValue);
     return 0;
