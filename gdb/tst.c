2a9ad5f5867772dd178e1461c0bf679e8722e674
philiptung <philip.dongfei@gmail.com>
2018-06-22 14:14:20 +0800

    add gdb demo

---
+++ gdb/tst.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int func(int n)
+{
+    int sum=0, i;
+    int *array = (int*)malloc(n * sizeof(int));
+    for (i=0; i<n; i++)
+    {
+        array[i] = i;
+        sum += i;
+    }
+    free(array);
+
+    return sum;
+}
+
+int main()
+{
+    int i; 
+    long result = 0;
+    for (i=1; i<=100; i++)
+        result += i;
+
+    printf("result[1-100] = %i \n", result);
+    printf("result[1-250] = %d \n", func(250));
+}
+
