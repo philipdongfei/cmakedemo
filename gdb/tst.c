a931b3b4ec885e3f06966d99d9a876b5b816fa1f
philiptung <philip.dongfei@gmail.com>
2018-07-02 13:38:43 +0800

    ap unix demo

--- gdb/tst.c
+++ gdb/tst.c
@@ -22,7 +22,7 @@ int main()
     for (i=1; i<=100; i++)
         result += i;
 
-    printf("result[1-100] = %i \n", result);
+    printf("result[1-100] = %i \n", (int)result);
     printf("result[1-250] = %d \n", func(250));
 }
 
