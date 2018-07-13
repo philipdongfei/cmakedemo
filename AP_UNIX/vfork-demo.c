06b0ed5f30ab7610324991946a7115ca00dbe0ca
philiptung <philip.dongfei@gmail.com>
2018-07-13 17:18:22 +0800

    control fork

---
+++ AP_UNIX/vfork-demo.c
@@ -0,0 +1,25 @@
+#include <sys/types.h>
+#include "ourhdr.h"
+
+int     glob = 6;
+
+int main(void)
+{
+    int    var;
+    pid_t  pid;
+
+    var = 88;
+    printf("before vfork\n");
+
+    if ((pid = vfork()) < 0)
+        err_sys("vfork error");
+    else  if (pid == 0) {
+        glob++;
+        var++;
+        exit(0);//_exit(0);
+    }
+    /*parent */
+    printf("pid = %d, glob = %d, var = %d\n", getpid(), glob, var);
+    exit(0);
+}
+
