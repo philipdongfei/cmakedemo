4ee7b17b212970aca132cbe760d5afce6c37a58d
philiptung <philip.dongfei@gmail.com>
2018-08-09 15:07:48 +0800

    term 2

--- AP_UNIX/term-func.c
+++ AP_UNIX/term-func.c
@@ -6,9 +6,16 @@
 #include    <dirent.h>
 #include    <limits.h>
 #include    <unistd.h>
+#include    <signal.h>
 
 #define DEV     "/dev/"   /* device directory */
 #define DEVLEN  sizeof(DEV)-1   /* sizeof includes null at end */
+
+#define  MAX_PASS_LEN    8   /* max #chars for user to enter  */
+
+static struct termios    save_termios;
+static int               ttysavefd = -1;
+static enum { RESET, RAW, CBREAK } ttystate = RESET;
 
 static char ctermid_name[L_ctermid];
 
@@ -62,5 +69,125 @@ ttyname(int fd)
     }
     closedir(dp);
     return (rval);
+}
+
+
+char *
+getpass(const char *prompt)
+{
+    static char    buf[MAX_PASS_LEN + 1];    /* null byte at end */
+    char           *ptr;
+    sigset_t       sig, sigsave;
+    struct termios term, termsave;
+    FILE           *fp;
+    int            c;
+
+    if ((fp = fopen(ctermid(NULL), "r+")) == NULL)
+        return (NULL);
+    setbuf(fp, NULL);
+
+    sigemptyset(&sig);    /* block SIGINT & SIGTSTP, save signal mask */
+    sigaddset(&sig, SIGINT);
+    sigaddset(&sig, SIGTSTP);
+    sigprocmask(SIG_BLOCK, &sig, &sigsave);
+
+    tcgetattr(fileno(fp), &termsave);  /* save tty state */
+    term = termsave;                   /* structure copy */
+    term.c_lflag &= ~(ECHO | ECHOE | ECHONL);
+    tcsetattr(fileno(fp), TCSAFLUSH, &term);
+
+    fputs(prompt, fp);
+
+    ptr = buf;
+    while ((c = getc(fp)) != EOF && c != '\n') {
+        if (ptr < &buf[MAX_PASS_LEN])
+            *ptr++ = c;
+
+    }
+    *ptr = 0;      /* null terminate */
+    putc('\n', fp);   /* we echo a newline */ 
+
+                    /* restore tty state */
+    tcsetattr(fileno(fp), TCSAFLUSH, &termsave);
+
+                    /* restore signal mask */
+    sigprocmask(SIG_SETMASK, &sigsave, NULL);
+    fclose(fp);     /* done with /dev/tty */
+    return (buf);
+}
+
+int
+tty_cbreak(int fd)   /*  put terminal into a cbreak mode */
+{
+    struct termios  buf;
+    
+    if (tcgetattr(fd, &save_termios) < 0)
+        return (-1);
+
+    buf = save_termios;   /* structure copy */
+    buf.c_lflag &= ~(ECHO | ICANON);
+                     /* echo off, canonical mode off */
+    buf.c_cc[VMIN] = 1; /* Case B: 1 byte at a time, no timer */
+    buf.c_cc[VTIME] = 0;
+
+    if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
+        return (-1);
+    ttystate = CBREAK;
+    ttysavefd = fd;
+    return (0);
+}
+
+int 
+tty_raw(int fd)   /* put terminal into a raw mode */
+{
+    struct termios  buf;
+    if (tcgetattr(fd, &save_termios) < 0)
+        return (-1);
+
+    buf = save_termios;  /* structure copy */
+    buf.c_lflag &= ~(ECHO | ICANON | IEXTEN | ISIG);
+                        /* ehco off, canonical mode off, extended input
+                         * processing off, signal chars off */
+    buf.c_iflag &= ~(BRKINT | ICRNL | INPCK | ISTRIP | IXON);
+                       /* no SIGINT on BREAK, CR-to-NL off, input parity
+                        * check off, don't strip 8th bit on input,
+                        * output flow control off */
+    buf.c_cflag &= ~(CSIZE | PARENB);
+                       /* clear size bits, parity checking off */
+    buf.c_cflag |= CS8;
+                       /* set 8 bits/char */
+    buf.c_oflag &= ~(OPOST);
+                      /* output processing off */
+    buf.c_cc[VMIN] = 1; /* Case B: 1 byte at a time, no timer */
+    buf.c_cc[VTIME] = 0;
+
+    if (tcsetattr(fd, TCSAFLUSH, &buf) < 0)
+        return (-1);
+    ttystate = RAW;
+    ttysavefd = fd;
+    return(0);
+}
+
+int tty_reset(int fd)    /* restore terminal's mode */
+{
+    if (ttystate != CBREAK && ttystate != RAW)
+        return (0);
+    if (tcsetattr(fd, TCSAFLUSH, &save_termios) < 0)
+        return (-1);
+    ttystate = RESET;
+    return (0);
+}
+
+void 
+tty_atexit(void)    /* can be set up by atexit(tty_atexit) */
+{
+    if (ttysavefd >= 0)
+        tty_reset(ttysavefd);
+}
+
+struct termios *
+tty_termios(void)   /* let caller see original tty state */
+{
+    return (&save_termios);
 }
 
